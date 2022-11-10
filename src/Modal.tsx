0acc136326ced8680580f117ab14767fd9924814
laurent-martinez <martilaurier@gmail.com>
2022-11-10 17:30:21 +0100

    update customs & css on modal

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Container = styled.div`
-  position : absolute;
+  position: absolute;
   top: 0;
   left: 0;
   width: 100vw;
@@ -11,7 +11,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 const Overlay = styled.div`
   position: absolute;
   top: 0;
@@ -22,73 +22,78 @@ const Overlay = styled.div`
   height: 100%;
   background: rgb(2 2 2 / 63%);
   z-index: 998;
-`
+`;
 const Modal = styled.div`
   background-color: #fff;
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: space-evenly;
   position: absolute;
   top: 40%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 50%;
+  width: 35%;
   z-index: 999;
-  height: 300px;
   border-radius: 5px;
+  overflow: hidden;
 
-.closeBtn {
-  padding: 0.3rem 0.5rem;
-  border: none;
-  border-radius: 5px;
-  position: absolute;
-  top: 0.8rem;
-  right: 0.8rem;
-  background-color: #93ad18;
-  color: #fff;
-  cursor: pointer;
-}
+  .closeBtn {
+    padding: 0.2rem 0.5rem;
+    border: none;
+    border-radius: 5px;
+    position: absolute;
+    margin-bottom: 2rem;
+    top: 0.8rem;
+    right: 0.8rem;
+    color: #fff;
+    cursor: pointer;
+  }
 
-.modalImg {
-width: 100%;
-display: flex;
-justify-content: center;
-align-items: center;
-}
-.modalImg img {
-  width: 20%;
-  height: max-content;
-  object-fit: contain;
-}
-
-.modalContent {
-  text-align: center;
-}
-`
+  .modalImg {
+    width: 100%;
+  }
+  .modalImg img {
+    width: 100%;
+    height: 150px;
+    object-fit: cover;
+  }
+`;
 export interface ModalProps {
-    img?: string;
-    children: React.ReactNode;
-    open: boolean;
-    hideModal: ()=> boolean;
-  }
+  img: string;
+  children: React.ReactNode;
+  closeBtnStyle: string;
+  open: boolean;
+  styledContent: string;
+  hideModal: () => boolean;
+}
 
-export function MyModal({children,img,open,hideModal}: ModalProps) {
-    if (!open) return null;
-  
-    return (
-      <Container>
-        <Overlay>
-          <Modal>
-            <div className="modalImg">
-              <img src={img} alt="" />
-            </div>
-            <div className="modalContent">{children}</div>
-            <button type="button" className="closeBtn" onClick={hideModal}>
-              X
-            </button>
-          </Modal>
-        </Overlay>
-      </Container>
-    );
-  }
+export function SublimeModal({
+  children,
+  img,
+  open,
+  hideModal,
+  styledContent,
+  closeBtnStyle,
+}: ModalProps) {
+  if (!open) return null;
+
+  return (
+    <Container>
+      <Overlay>
+        <Modal>
+          <button
+            type="button"
+            className={`closeBtn ${closeBtnStyle}`}
+            onClick={hideModal}
+          >
+            X
+          </button>
+          <div className="modalImg">
+            <img src={img} alt="" />
+          </div>
+          <div className={styledContent}>{children}</div>
+        </Modal>
+      </Overlay>
+    </Container>
+  );
+}
